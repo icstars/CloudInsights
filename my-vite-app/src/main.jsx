12260becc799de6270777dea1c2aa7039ce193bf
Jasmine Valladares <jvalladares@icstars.org>
2025-01-23 15:09:33 -0600

    Test

--- my-vite-app/src/main.jsx
+++ my-vite-app/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Nameyouwant from 'c:/Users/ShemarHouston/Documents/okay/my-vite-app/src/nameyouwant'
+//import Nameyouwant from 'c:/Users/ShemarHouston/Documents/okay/my-vite-app/src/nameyouwant'
 
 
 createRoot(document.getElementById('root')).render(
