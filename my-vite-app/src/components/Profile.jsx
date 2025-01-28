7e6d095f01e2b26940a29ed7cc269917616af9ab
Shemar Houston <shouston@icstars.org>
2025-01-28 14:34:30 -0600

    bio with updated buttons

--- my-vite-app/src/components/Profile.jsx
+++ my-vite-app/src/components/Profile.jsx
@@ -7,11 +7,16 @@ function Profile(){
             <img src={Pfp} alt="Lily"className="lily" />
             <p className="pro"></p>
             
-            <p className="bio">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p className="bio"><ol className="title">Lilly Moore -
+Health Navigator</ol>
+ 
+       
+📞 (555) 123-4567<br/>
+📧 lilly.johnson@email.com<br/>
+📍 In-Field<br/>
+Monday - Friday: 9:00 AM - 6:00 PM</p> 
+
+
 
 
 
